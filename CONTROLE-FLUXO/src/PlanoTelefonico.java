15f2b66c3d7df8e197efa254127d2f92d86d25a5
caio_cesena <caio.celou@gmail.com>
2024-06-06 10:20:34 -0300

    Controle de Fluxo com Else If e Switch case

---
+++ CONTROLE-FLUXO/src/PlanoTelefonico.java
@@ -0,0 +1,20 @@
+public class PlanoTelefonico {
+    public static void main(String[] args) {
+        String plano = "T";
+
+        switch (plano) {
+            case "T":{
+                System.out.println("5Gb de YouTube");
+            }
+            case "M":{
+                System.out.println("Whats e instagram grátis");
+            }
+            case "B":{
+                System.out.println("100 minutos de ligção");
+            }
+                
+            default:
+                break;
+        }
+    }
+}
---
+++ CONTROLE-FLUXO/src/ResultadoEscolar.java
@@ -0,0 +1,31 @@
+public class ResultadoEscolar {
+    public static void main(String[] args) {
+        /* //Estrutura else if
+        int nota =6;
+
+        if(nota >= 7)
+            System.out.println("Aprovado");
+        
+        else if(nota >= 5 && nota <7)
+            System.out.println("Prova de recuperação");
+
+        else
+            System.out.println("Reprovado");*/
+
+        //Condição ternaria 
+/* 
+        int nota = 10;
+
+        String resultado = nota >=7 ? "Aprovado" : "Reprovado";
+
+        System.out.println(resultado);
+*/
+
+        int nota = 4;
+
+        String resultado = nota >=7 ? "Aprovado" : nota >=5 && nota <7 ? "Recuperação": "Reprovado";
+
+        System.out.println(resultado);
+
+    }
+}
